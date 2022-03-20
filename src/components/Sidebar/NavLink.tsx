9f91c64d6ef453a00a04907edfc82d70d29e00b3
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-03-20 00:36:16 -0300

    Ajustes componentização sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,14 +1,19 @@
-import { Icon, Link, Text } from "@chakra-ui/react";
+import {
+  Icon,
+  Link,
+  Text,
+  LinkProps as ChakraLinkProps,
+} from "@chakra-ui/react";
 import { ElementType } from "react";
 
-interface NavLinkProps {
+interface NavLinkProps extends ChakraLinkProps {
   children: string;
   icon: ElementType;
 }
 
-export function NavLink({ children, icon }: NavLinkProps) {
+export function NavLink({ children, icon, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" py="1" alignItems="center">
+    <Link display="flex" py="1" alignItems="center" {...rest}>
       <Icon as={icon} fontSize="20" />
       <Text ml="4" fontWeight="medium">
         {children}
